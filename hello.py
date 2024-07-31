0dc1e0ccff221197a40deaec611042682bd148a5
Stephane ASSOCLE <mensste30@gmail.com>
2024-07-31 11:16:22 +0100

    set erreur 400 and 500 page

---
+++ hello.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#Index route
+@app.route("/")
+def index():
+    return "Thank you God for all !!!"
+
+#Invalid URL
+@app.errorhandler(404)
+def page_not_found(e) :
+    return render_template("400.html"), 404
+
+#Server error
+@app.errorhandler(500)
+def page_not_found(e) :
+    return render_template("500.html"), 500
+
+if __name__ == '__main__' : 
+    app.run(debug=True)
