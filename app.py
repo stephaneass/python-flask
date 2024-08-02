568dc6839f58d8734bfb4e6800e6edded145b8b5
Stephane ASSOCLE <mensste30@gmail.com>
2024-08-02 11:14:48 +0100

    name forme

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "My Super Secret Key"
+
+# Create a Form Class
+class NamerForm(FlaskForm) :
+    name = StringField("What is your name ?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 #Index route
 @app.route("/")
@@ -11,6 +20,17 @@ def index():
 @app.route("/user/<name>")
 def user_profile(name):
     return render_template("profile.html", name=name)
+
+@app.route("/name", methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+
+    return render_template("name.html", name = name, form = form)
 
 #Invalid URL
 @app.errorhandler(404)
