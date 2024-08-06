12a7face186003b3b400ac612e8d3cdaf0c7276e
Stephane ASSOCLE <mensste30@gmail.com>
2024-08-06 08:58:21 +0100

    add posts models and migration

--- app.py
+++ app.py
@@ -64,6 +64,14 @@ class Users(db.Model):
     def __repr__(self):
         return '<Name %r>' % self.name
 
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    slug = db.Column(db.String(255))
+
 #Index route
 @app.route("/")
 def index():
